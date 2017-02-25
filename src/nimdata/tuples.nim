052bc2830138516a5574c36ebb7e595d02b82457
Fabian Keller <github.100.fkeller@spamgourmet.com>
2017-02-25 22:48:31 +0100

    implemented projectTo macro

--- src/nimdata/tuples.nim
+++ src/nimdata/tuples.nim
@@ -1,52 +1,89 @@
 import macros
+import typetraits
 
 import utils
 
-macro projectTo*(t: typed, fields: varargs[untyped]): untyped =
-  echo "t = ", t.repr
-  let tType = t.getType               # returns the type as a NimNode
-  let tKind = tType.kind              # note this is not the same as typeKind (bracket for a tuple)
-  let tTypeKind = tType.typeKind
 
-  echo "t.getType = ", t.getType.repr
-  echo "t.getType.typeKind = ", tTypeKind
-  echo "fields = ", fields.repr
-  echo fields.treeRepr
+macro projectTo*(t: typed, fields: varargs[untyped]): untyped =
+
+  if fields.len == 0:
+    error "At least one field required to project to."
+
+  let tType = t.getType               # returns the type as a NimNode
+  let tTypeKind = tType.typeKind
 
   if not (tTypeKind in {ntyTuple, ntyObject}):
     error "projectTo expects a tuple or object, but received: " & t.repr &
           " which has typeKind " & tTypeKind.repr
 
+  # extract fields present in given tuple
+  var existingFieldNames = newSeq[string]() # actually we should switch to hashset...
+  let tTypeInst = t.getTypeInst
+  for child in tTypeInst.children:
+    if child.kind != nnkIdentDefs:
+      error "projectTo expects a tuple or object, consisting of nnkIdentDefs children."
+    else:
+      let field = child[0] # first child of IdentDef is a Sym corresponding to field name
+      existingFieldNames.add($field)
+
+  # iterate over fields to project to
+  result = newPar()
   for field in fields:
-    echo field.treeRepr
     if field.kind != nnkIdent:
       error "projectTo expects pure identifiers as varargs, but received: " &
             field.repr & " which is of kind " & $field.kind
 
-    let tupleAccess = quote do: `t`.`field`
-    echo tupleAccess.repr
-    when compiles(tupleAccess):
-      echo "compiles"
+    if $field in existingFieldNames:
+      # create the `t.field` expression
+      let fieldExpr = newDotExpr(t, field)
+      # and add `field: t.field` to the Par experession
+      result.add(
+        newColonExpr(newIdentNode($field), fieldExpr)
+      )
     else:
-      echo "does NOT compile"
-
-  result = quote do:
-    1
+      error "Can't project to " & $field & " (does not exist in source tuple)"
 
 
 
 UnitTestSuite("Tuples"):
 
-  proc genTuple(): tuple[a: int, b: string] =
+  proc genTupleFromProc(): tuple[a: int, b: string] =
     result = (a: 1, b: "2")
 
   test "projectTo":
 
-    let t = genTuple() # (name: "A", age: 99)
-    echo t.projectTo(name)
-    echo t.projectTo(name, ages)
+    block:
+      let t = (name: "A", age: 99)
+      check: t.projectTo(name, age) == (name: "A", age: 99)
+      check: t.projectTo(name) == (name: "A")
+      check: t.projectTo(age) == (age: 99)
 
-    check true
+    block:
+      check: (name: "A", age: 99).projectTo(name, age) == (name: "A", age: 99)
+      check: (name: "A", age: 99).projectTo(name) == (name: "A")
+      check: (name: "A", age: 99).projectTo(age) == (age: 99)
 
+    block:
+      let t = genTupleFromProc()
+      check: t.projectTo(a, b) == (a: 1, b: "2")
+      check: t.projectTo(a) == (a: 1)
+      check: t.projectTo(b) == (b: "2")
 
+    block:
+      check: genTupleFromProc().projectTo(a, b) == (a: 1, b: "2")
+      check: genTupleFromProc().projectTo(a) == (a: 1)
+      check: genTupleFromProc().projectTo(b) == (b: "2")
+
+    block:
+      let t = (name: "A", age: 99, height: 200.0)
+      check: t.projectTo(name, age, height).projectTo(name, age, height) == t
+      check: t.projectTo(name, age).projectTo(age) == (age: 99)
+
+    block:
+      let t = (name: "A", age: 99, height: 200.0)
+      check: notCompiles: t.projectTo()
+      check: notCompiles: t.projectTo(nonExisting)
+      check: notCompiles: t.projectTo(asdf asdf)
+      check: notCompiles: t.projectTo(name, age, name)
+      check: notCompiles: t.projectTo(name, age).projectTo(height)
 
