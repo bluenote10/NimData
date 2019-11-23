49c8a0792a03cad86706306d0cbe121d5d1e6b29
bflanders <benjaminjflanders@gmail.com>
2019-11-23 13:54:10 -0500

    Create one DF from a file pattern.

--- src/nimdata.nim
+++ src/nimdata.nim
@@ -849,6 +849,24 @@ method iter*(df: FileRowsGZipDataFrame): (iterator(): string) =
       yield res
     stream.close()
 
+# -----------------------------------------------------------------------------
+# FileRows (gzip)
+type
+  FilesRowsGZipDataFrame = ref object of DataFrame[string]
+    filename: string
+    hasHeader: bool
+
+method iter*(df: FilesRowsGZipDataFrame): (iterator(): string) =
+  result = iterator(): string =
+    for filename in walkFiles(df.file_pattern):
+      var stream = newGZipStream(filename)
+      var res = TaintedString(newStringOfCap(80))
+      # Discard header
+      if df.hasHeader:
+        discard stream.readLine(res)
+      while stream.readLine(res):
+        yield res
+      stream.close()
 
 # -----------------------------------------------------------------------------
 # Smart from file construction
@@ -890,6 +908,11 @@ proc fromFile*(dfc: DataFrameContext,
         hasHeader: hasHeader
       )
 
+# Read files from pattern - not generic yet
+# TODO: make this more generic like proc fromFiles
+proc fromGZFiles*(dfc: DataFrameContext, file_pattern: string): DataFrame[string] =
+  result = FilesRowsGZipDataFrame(file_pattern: file_pattern, hasHeader: true)
+  
 # -----------------------------------------------------------------------------
 # HDF5
 # -----------------------------------------------------------------------------
